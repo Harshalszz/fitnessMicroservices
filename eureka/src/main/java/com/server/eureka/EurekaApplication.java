d04dac7bc811f0cb35c5e84e8d099a28d02689f5
Harshalszz <477harshz@gmail.com>
2025-08-04 17:09:54 +0530

    setup the eureka server for micro services registry

---
+++ eureka/src/main/java/com/server/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.server.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+// eureka server is like a telephone directory where micro serviese can check which microservice is ruuniing on which port , IP address
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
